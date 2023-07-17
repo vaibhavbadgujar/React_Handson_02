7cd57bc0d69a68b22fc953e52add7c44e32b75b2
vaibhavbadgujar <91780179+vaibhavbadgujar@users.noreply.github.com>
2023-07-17 20:02:16 +0530

    React_Handson_02

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './code/style.css';
+import Form from './code/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/code/Form.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import './style.css';
+
+const Form = () => {
+
+    const [formState, setFormState] = useState({
+        name:"",
+        department:"",
+        rating:"",
+        data:[]
+    });
+
+    const handleOnChange = (event) => {
+        const {name, value} = event.target;
+        setFormState({...formState, [name]:value});
+    };
+
+    const buttonFunc = (event) => {
+        event.preventDefault();
+        const saveData = {
+            name: formState.name,
+            department: formState.department,
+            rating: formState.rating
+        }
+        const arr = formState.data;
+        arr.push(saveData);
+        setFormState({data : arr});
+    };
+
+    
+    return(
+        <div>
+            <h1>Employee Feedback Form</h1>
+            <form>
+                <label> Name : &nbsp; &nbsp;
+                    <input type={"text"} name={"name"} value={formState.name} onChange={handleOnChange} />
+                </label>
+                <br/><br/>
+                <label> Department : &nbsp; &nbsp;
+                    <input type={"text"} name={"department"} value={formState.department} onChange={handleOnChange} />
+                </label>
+                <br/><br/>
+                <label> Rating : &nbsp; &nbsp;
+                    <input type={"number"} name={"rating"} value={formState.rating} onChange={handleOnChange} />
+                </label>
+                <br/><br/><br/><br/>
+                <button type="submit" onClick={buttonFunc}>Submit</button>
+            </form>
+            <div className="dataBox">
+            {
+                formState.data.map( (item,index)=>{
+                return(
+                    
+                        <div className="displayBox">
+                            <span>Name: {item.name} | </span>
+                            <span>Department: {item.department} | </span>
+                            <span>Rating: {item.rating}</span>
+                        </div>
+                );
+                })
+            }
+            </div>
+        </div>
+    );
+}
+
+export default Form;
